11f3e57a8dab70b4591ade9453bf65d40f006dad
Mika Turunen <mika.turunen@ymail.com>
2015-02-07 17:17:11 +0000

    Initial assets

---
+++ gulpfile.js
@@ -0,0 +1,84 @@
+/**
+ * Gulp usage file for the whole project. 
+ * Usage cases: Compiling all the typescript files.
+ */
+ 
+"use strict";
+
+var gulp = require("gulp");
+var ts = require("gulp-typescript");
+var eventStream = require("event-stream");
+var jade = require("gulp-jade");
+var path = require("path");
+var tslint = require("gulp-tslint");
+var uglify = require("gulp-uglify");
+var sourcemaps = require("gulp-sourcemaps");
+var sequence = require("run-sequence");
+var browserify = require("browserify");
+var tsify = require("tsify");
+var source = require("vinyl-source-stream");
+var buffer = require("vinyl-buffer");
+
+var typeDefinitions = [];
+var projectDefinitions = [ ];
+
+var fromDefinitelyTypedClient = [
+    "definitions/**/*.d.ts"
+];
+
+var typeDefinitionsClient = fromDefinitelyTypedClient
+    .concat(projectDefinitions)
+    .concat([ "source/**/*.ts" ]);
+
+// TASK NAMES
+var taskTslintClient = "tslint-client";
+var taskTsc = "ts";
+var taskTscClient = "ts-client";
+var taskUglifyJs = "uglify-js";
+var taskBrowserifyClient = "browserify";
+
+var tscClientOptions = {
+    declarationFiles: true,
+    noExternalResolve: false,
+    noImplicitAny: true,
+    removeComments: true,
+    target: "ES5",
+    module: "amd",
+    showErrors: true
+};
+
+gulp.task(taskTslintClient, function() {
+    console.log("Linting Client side TS: " + JSON.stringify(typeDefinitionsClient, null, 2));
+    return gulp.src(typeDefinitionsClient).pipe(tslint()).pipe(tslint.report("verbose"));
+})
+
+gulp.task(taskBrowserifyClient, function() {
+    var bundler = browserify({
+        entries: [ 
+            path.join(__dirname, "node_modules/phaser/typescript/phaser.d.ts"), 
+            path.join(__dirname, "source/definitions/client.d.ts"), 
+            path.join(__dirname, "source/index.ts") 
+        ],
+        debug: false
+    });
+
+    var bundle = function() {
+        return bundler
+            .plugin("tsify", { noImplicitAny: true, removeComments: true, declarationFiles: true })
+            .bundle()
+            .pipe(source( path.normalize("index.min.js") ))
+            .pipe(buffer())
+            .pipe(uglify())
+            .pipe(gulp.dest("source"));
+    };
+
+    return bundle();
+});
+
+// defining the tasks gulp runs -- in default we do basically all the tasks in one
+gulp.task("default", function() {
+    sequence(
+            [ taskTslintClient, taskBrowserifyClient ],
+            taskBrowserifyClient
+        );
+});
