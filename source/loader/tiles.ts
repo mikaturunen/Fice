ef955328634d4e6d8b8a228baf2ba4b6d0304ef8
Mika Turunen <mika.turunen@ymail.com>
2015-02-07 17:49:58 +0000

    Stub loader that does nothing

---
+++ source/loader/tiles.ts
@@ -0,0 +1,19 @@
+"use strict";
+
+/** 
+ * Used to load the tile based levels into the Phaser engine
+ * @module TileLoader
+ */
+module TileLoader = {
+    
+    /**
+     * @param {TileBasedLevel} level - Level to load into the engine as the active level.
+     */
+    load: (level: TileBasedLevel) => {
+        level.tileLayers.forEach((layer: TileLayer) => {
+            
+        });    
+    }
+};
+
+export = TileLoader;
