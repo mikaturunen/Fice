f6e8c28780f5760a4d162add7f1e27a039a99403
Mika Turunen <mika.turunen@ymail.com>
2015-02-08 07:47:15 +0000

    Resizing to the original resolution and rendering the layers from Tiled data

--- source/index.ts
+++ source/index.ts
@@ -17,11 +17,14 @@ var level = (levelName: string) => {
 var init = () => {
     var map: Phaser.Tilemap; 
     var layer: Phaser.TilemapLayer;
+    var backgroundLayer: Phaser.TilemapLayer;
     
     /** 
      * Start Phaser itself
      */
-    var game = new Phaser.Game(800, 600, Phaser.AUTO, "FIce", { 
+    var game = new Phaser.Game(16 * 32, 14 * 32, Phaser.AUTO, "FIce", { 
+        // Wacky resolution? Yes, I'm going currently for the remake of the original so..
+        
         preload: () => {
             // Loading all the game related assets
             game.load.tilemap("level", level("lvl.json"), null, Phaser.Tilemap.TILED_JSON);
@@ -34,7 +37,10 @@ var init = () => {
             // Creating the actual map from the tiled data
             map = game.add.tilemap("level");
             map.addTilesetImage("tiles", "tiles");
+            // Creates the layer from the collisions layer in the Tiled data
             layer = map.createLayer("collision");
+            // Creating the background layer from the layer "background" in the Tiled data
+            backgroundLayer = map.createLayer("background");
             // Makes sure the game world matches the layer dimensions
             layer.resizeWorld();
         }
