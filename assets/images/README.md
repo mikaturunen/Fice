9c5a13a8060d2a736df65d460a5a84d86b418b76
Mika Turunen <mika.turunen@ymail.com>
2015-02-08 07:25:44 +0000

    Fixed definition references and added node.d.ts to references too (require)

---
+++ assets/images/README.md
@@ -0,0 +1,7 @@
+# Images
+The following images have been downloaded from http://opengameart.org/content/platform-pixel-art-assets and are under the following Creative Commons licensing: http://creativecommons.org/publicdomain/zero/1.0/
+
+- items.png
+- tiles.png 
+
+
--- source/definitions/client.d.ts
+++ source/definitions/client.d.ts
@@ -1,3 +1,6 @@
 // I'm lazy, reference to all the *.d.ts files
 
-///<reference path="tile-based-levels.d.ts" />
+///<reference path="../../node_modules/phaser/typescript/phaser.d.ts" />
+
+///<reference path="tile-based-level.d.ts" />
+///<reference path="DefinitelyTyped/node.d.ts" />
