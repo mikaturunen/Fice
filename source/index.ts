5549cfa555873716eedcffecdd5e1c24a524c38f
Mika Turunen <mika.turunen@cybercom.com>
2015-02-12 18:17:30 +0200

    Another failed attempt at tweening the character... this is horrible :D

--- source/index.ts
+++ source/index.ts
@@ -80,7 +80,8 @@ var layers: { [ layerName: string ]: Phaser.TilemapLayer; } = { };
 var blockGroup: Phaser.Group;
 var targetGroup: Phaser.Group;
 var player: Phaser.Sprite;
-var playerTween: any;
+var playerTween: any = { };
+var speedInMilliseconds: number = 500;
 
 var loadLayers = (map: any) => {
     [
@@ -111,7 +112,7 @@ var fillSpriteGroup = (spriteGroup: Phaser.Group, type: string, layer: string, f
 
 var createTweenForPlayer = (velocity: number, nextPosition: Phaser.Point, game: any) => {
     // 'totalMovementTimeToNextTile' seconds to travel 'tileSizes.width'
-    var totalMovementTimeToNextTile: number = 500;
+    var totalMovementTimeToNextTile: number = speedInMilliseconds;
     var newPosition: Phaser.Point = getFlooredWorldCoordinateFromWorldCoordinates(
         player.body.x + velocity,
         player.body.y,
@@ -121,10 +122,10 @@ var createTweenForPlayer = (velocity: number, nextPosition: Phaser.Point, game: 
     nextPosition.y = newPosition.y;
 
     var duration: number =
-        (game.physics.arcade.distanceToXY(player, newPosition.x, newPosition.y) / tileSizes.width) *
+        (game.physics.arcade.distanceToXY(player, newPosition.x, newPosition.y) / velocity) *
         totalMovementTimeToNextTile;
 
-    var tween = game.add.tween(player).to(newPosition, duration, Phaser.Easing.Linear.None, true);
+    var tween = game.add.tween(player.body.velocity).to({ x: velocity }, duration, Phaser.Easing.Linear.None, true);
     tween.start();
 
     console.log("UPDATE", duration, playerTween, player.body.x, player.body.y, newPosition);
@@ -186,15 +187,31 @@ var init = () => {
         },
 
         update: () => {
-            if (playerTween && playerTween.isRunning && game.time.totalElapsedSeconds() - moveStartTimer >= 0.5) {
-                console.log("Tween running...");
-                playerTween.stop();
-                // attempting to tell the player that you've reached your destination, HALT!
-                player.body.x = nextPosition.x;
-                player.body.y = nextPosition.y;
-            } else {
-                player.body.velocity.x = 0;
+            var currentMovementTimeDelta: number = (game.time.totalElapsedSeconds() - moveStartTimer) * 1000;
+            if (playerTween.isRunning === true) {
+                // Actively moving
+                if (currentMovementTimeDelta < speedInMilliseconds) {
+                    console.log("Tween running...", currentMovementTimeDelta, speedInMilliseconds);
+                } else {
+                    console.log("STOPPED...");
+                    playerTween.stop();
+                    console.log("player, next", player.body.x, player.body.y, nextPosition.x, nextPosition.y);
 
+                    // attempting to tell the player that you've reached your destination, HALT!
+                    player.body.x = nextPosition.x;
+                    player.body.y = nextPosition.y;
+                    player.body.velocity.x = player.body.velocity.y = 0;
+                }
+            } else {
+                if (playerTween.isRunning === false) {
+                    // attempting to tell the player that you've reached your destination, HALT!
+                    player.body.x = nextPosition.x;
+                    player.body.y = nextPosition.y;
+                    player.body.velocity.x = player.body.velocity.y = 0;
+                }
+
+                console.log("playerTween", playerTween !== undefined, ", running:", playerTween ? playerTween.isRunning : "");
+                // Stopped, ready for new movement action
                 if (game.input.keyboard.isDown(Phaser.Keyboard.LEFT)) {
                     moveStartTimer = game.time.totalElapsedSeconds();
                     playerTween = createTweenForPlayer(-tileSizes.width, nextPosition, game);
