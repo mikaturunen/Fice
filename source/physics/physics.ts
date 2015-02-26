86b76ccd3a7af8dee7eb4396d30f639f5fe149db
Mika Turunen <mika.turunen@ymail.com>
2015-02-26 19:51:37 +0200

    Falling in place now... minor bugs still here x3

--- source/physics/physics.ts
+++ source/physics/physics.ts
@@ -1,6 +1,7 @@
 
 import constant = require("../utilities/constants");
 import utilities = require("../utilities/utilities");
+import world = require("../world/tiles");
 
 function isMoving(body: PhysicsBody) {
     return  body.velocity.x >=  constant.VelocityTreshold || 
@@ -38,7 +39,25 @@ module physics {
             move(body);
 
             if (isMoving(body)) {
+                // Body is moving, we'll store it so we can notify the module that we still do have moving bodies
                 movingBodies.push(body);
+            } else {
+                // Body is not moving, apply gravity to it to see if it starts falling as it has reached the 
+                // potentially new tile.
+                
+                // Calculate a position from current sprites center to the center of one tile below and then floor the
+                // value.
+                var x: number = body.x + (constant.TileSize.width / 2);
+                var y: number = body.y + (constant.TileSize.heigth * 1.5);
+                var tile: Phaser.Tile = world.map.getTileWorldXY(x, y, constant.TileSize.width, 
+                    constant.TileSize.heigth, "collision");
+
+                if (!tile) {
+                    body.velocity.y = constant.Velocity * game.time.elapsed;
+                    movingBodies.push(body);
+                    body.next.x = Math.floor(x / constant.TileSize.width) * constant.TileSize.width;
+                    body.next.y = Math.floor(y / constant.TileSize.heigth) * constant.TileSize.heigth;
+                }
             }
         });
         
