1088e967bd623ef828476154dbe53575ac096354
Mika Turunen <mika.turunen@ymail.com>
2015-03-22 13:39:06 +0200

    Work started on player animations.

--- source/entities/player.js
+++ source/entities/player.js
@@ -11,16 +11,59 @@ var keys = {
     shoot: phaserStorage.game.input.keyboard.addKey(Phaser.Keyboard.S)
 };
 
-var update = () => {
-    checkInputs();
+var player = () => {
+    var game = phaserStorage.game;
+
+    // Loading player sprite
+    player.sprite = game.add.sprite(
+            game.width / 2,       // X 
+            10,                   // Y
+            "player",             // Sprite / sheet name
+            0                     // frame
+        );
+
+    // Giving all the loaded sprite objects arcade physics
+    game.physics.arcade.enable(player.sprite);
+    player.sprite.body.gravity.y = 1000;
+    player.sprite.body.maxVelocity.y = 400;
+    player.sprite.body.collideWorldBounds = true;
+
+    createAnimations();
 };
 
+/**
+ * Creates all the animations from the player sprite sheet for player movement.
+ */
+var createAnimations = () => {
+    // Left direction 
+    player.sprite.animations.add("leftWalking", [0, 1, 2, 3], 10, true);
+    player.sprite.animations.add("leftJumping", [], 10, true);
+    player.sprite.animations.add("leftIdle", [], 10, true);
+    player.sprite.animations.add("leftFalling", [], 10, true);
+
+    // Right direction
+    player.sprite.animations.add("rightWalking", [5, 6, 7, 8], 10, true);
+    player.sprite.animations.add("rightJumping", [], 10, true);
+    player.sprite.animations.add("rightIdle", [], 10, true);
+    player.sprite.animations.add("rightFalling", [], 10, true);
+};
+
+var update = () => {
+    checkInputs();
+    setAnimations();
+};
+
+/**
+ * Goes through the defined user inputs and manipulates the player velocity.
+ */
 var checkInputs = () => {
     player.sprite.body.velocity.x = 0;
 
     if (keys.cursors.left.isDown) {
+        player.facing = "left";
         player.sprite.body.velocity.x = -playerVelocity;
     } else if (keys.cursors.right.isDown) {
+        player.facing = "right";
         player.sprite.body.velocity.x = playerVelocity;
     }
 
@@ -32,6 +75,43 @@ var checkInputs = () => {
 };
 
 /**
+ * Sets the player into a N animation series.
+ */
+var setAnimations() => {
+    trySettingAnimationJumping();
+    trySettingAnimationFalling();
+    trySettingAnimationWalkingLeft();
+    trySettingAnimationWalkingRight();
+};
+
+var trySettingAnimationJumping = () => {
+    if (player.sprite.body.velocity.y < 0) {
+        // Jumping
+    }
+};
+
+var trySettingAnimationFalling = () => {
+    if (player.sprite.body.velocity.y > 0) {
+        // Falling
+    }
+};
+
+var trySettingAnimationWalkingLeft = () => {
+    if (player.sprite.body.velocity.y === 0 && player.sprite.body.velocity.x < 0) {
+        // Walking left
+        
+    }
+};
+
+var trySettingAnimationWalkingRight = () => {
+
+};
+
+var trySettingAnimationStanding = () => {
+
+}:
+
+/**
  * Player entity
  */
 var player = {
@@ -40,6 +120,16 @@ var player = {
      * @type {Phaser.Sprite}
      */
     sprite: undefined,
+
+    /**
+     * What direction the player is facing currently.
+     */
+    facing: "right",
+
+    /**
+     * Creates the player entity.
+     */
+    create: create,
 
     /**
      * Update handler for Player
--- source/game/create.js
+++ source/game/create.js
@@ -13,24 +13,7 @@ var create = () => {
 
     // Loading the player
     var player = require("../entities/player");
-
-    // Loading player sprite
-    player.sprite = game.add.sprite(
-            game.width / 2,       // X 
-            10,                   // Y
-            "player",             // Sprite / sheet name
-            0                     // frame
-        );
-
-    // Giving all the loaded sprite objects arcade physics
-    game.physics.arcade.enable(player.sprite);
-    player.sprite.body.gravity.y = 1000;
-    player.sprite.body.maxVelocity.y = 400;
-    
-    // Set common physics base for all objects that need it
-    player.sprite.body.collideWorldBounds = true;
-
-
+    player.create();
 };
 
 console.log("Create in place.");
